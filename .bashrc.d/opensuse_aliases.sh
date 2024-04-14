6dac9a2772048b629e6bb3702451b1d9c0e01f4d
Fredrik Siesing <4631265+siesing@users.noreply.github.com>
2024-04-14 18:26:38 +0200

    Remove opensuse_aliases

--- .bashrc.d/opensuse_aliases.sh
+++
@@ -1,9 +0,0 @@
-# zypper aliases
-alias refresh='sudo zypper ref'                          # refresh repositories
-alias upgrade='sudo zypper dup --allow-vendor-change'   # upgrade OpenSUSE Tumbleweed
-alias install='sudo zypper in'                          # install a package
-alias remove='sudo zypper rm --clean-deps'              # remove a package and unneeded dependencies
-alias search='sudo zypper se'                           # search for package
-alias info='sudo zypper if'                             # view package information
-alias list='sudo zypper search -i'                      # list installed packages
-alias orphans='sudo zypper packages --orphaned'         # list all orphaned packages
